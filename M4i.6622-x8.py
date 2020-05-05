35d11c94b16ef6b6739773c9208bf598f02bc571
Ryan-Red <45923095+Ryan-Red@users.noreply.github.com>
2020-05-05 16:09:25 -0400

    Fixed some bugs in the calculate function

--- M4i.6622-x8.py
+++ M4i.6622-x8.py
@@ -13,23 +13,23 @@ from spcm_tools import *
 import math
 import sys
 import numpy as np
-
-# random signal frequencies for up to four channels in Hertz
-# if you change these you might want to change SPC_SAMPLERATE, too
-adSignalFrequency_Hz = [ 35000, 11723, 27000, 9800 ]
+import time
 
 def vCalcNewData (pnBuffer, lNumCh, llSamplePos, llNumSamples):
     for lChIdx in range (0, lNumCh):
         for i in range (0, llNumSamples, 1):
             # calculate a sample and put the calculated value into the correct position in the DMA buffer
-            pnBuffer[i * lNumCh + lChIdx] = int16(int(32767*i)) # * math.sin (2.*math.pi*(llSamplePos + i)/(10000))))
+            pnBuffer[i * lNumCh + lChIdx] = int16(int(32767** math.sin (2.*math.pi* i/(10000))))
+
+
+
 
 
 class M4i6622:
-    def __init__(self, ReplayLength=16384, Loops=1):
+    def __init__(self, ReplayLength=16384, Loops=1, address=b'/dev/spcm0'):
         
         #Connect the Card
-        self.hCard = spcm_hOpen (create_string_buffer (b'/dev/spcm0'))
+        self.hCard = spcm_hOpen (create_string_buffer (address))
         
         Valid = self.checkCard()
         if Valid == False:
@@ -42,10 +42,10 @@ class M4i6622:
         spcm_dwSetParam_i64 (self.hCard, SPC_LOOPS, Loops) # replay memsize "Loops" times
 
         self.lSetChannels = int32 (0)
-        spcm_dwGetParam_i32 (hCard, SPC_CHCOUNT, byref (self.lSetChannels))
+        spcm_dwGetParam_i32 (self.hCard, SPC_CHCOUNT, byref (self.lSetChannels))
 
         self.lBytesPerSample = int32 (0)
-        spcm_dwGetParam_i32 (hCard, SPC_MIINST_BYTESPERSAMPLE,  byref (self.lBytesPerSample))
+        spcm_dwGetParam_i32 (self.hCard, SPC_MIINST_BYTESPERSAMPLE,  byref (self.lBytesPerSample))
         
         
 
@@ -62,18 +62,18 @@ class M4i6622:
         self.lSerialNumber = int32 (0)
         spcm_dwGetParam_i32 (self.hCard, SPC_PCISERIALNO, byref (self.lSerialNumber))
         self.lFncType = int32 (0)
-        spcm_dwGetParam_i32 (self.hCard, SPC_FNCTYPE, byref (lFncType))
+        spcm_dwGetParam_i32 (self.hCard, SPC_FNCTYPE, byref (self.lFncType))
 
-        sCardName = szTypeToName (lCardType.value)
-        if lFncType.value == SPCM_TYPE_AO:
-            print("Found: {0} sn {1:05d}\n".format(sCardName,lSerialNumber.value))
+        sCardName = szTypeToName (self.lCardType.value)
+        if self.lFncType.value == SPCM_TYPE_AO:
+            print("Found: {0} sn {1:05d}\n".format(sCardName,self.lSerialNumber.value))
             return True
         else:
             print("Code is for an M4i.6622 Card.\nCard: {0} sn {1:05d} is not supported.\n".format(sCardName,lSerialNumber.value))
             return False
 
 
-    def setSoftwareBuffer(self,NotifySize=128*1024,BufferSize=32*1024*1024,):
+    def setSoftwareBuffer(self,NotifySize=128*1024,BufferSize=32*1024*1024):
 
         # setup software buffer
         self.lNotifySize_bytes = int32(NotifySize) # Buffer to notify size of the packet
@@ -99,39 +99,39 @@ class M4i6622:
 
 
 
-    def vCalcNewData(self, channel=0):
-        for i in range (0, llNumSamples, 1):
-            # calculate a sample and put the calculated value into the correct position in the DMA buffer
-            pnBuffer[i * 4 + channel] = int16(int(32767*i)) # * math.sin (2.*math.pi*(llSamplePos + i)/(10000))))
-    
-    def calculate(self):
-        while True:
-            try:
-                qwSamplePos = 0
-                lNumAvailSamples = (qwBufferSize.value // lSetChannels.value) // lBytesPerSample.value
-                vCalcNewData (cast (pvBuffer, ptr16), lSetChannels.value, qwSamplePos, lNumAvailSamples)
+
+    def calculate(self, channel=0):
+
+        try:
+            qwSamplePos = 0
+            self.lNumAvailSamples = (self.qwBufferSize.value // self.lSetChannels.value) // self.lBytesPerSample.value
+            self.pnBuffer = cast (self.pvBuffer, ptr16)
+
+            print("inb4")
+            vCalcNewData (cast (self.pvBuffer, ptr16), self.lSetChannels.value, qwSamplePos, self.lNumAvailSamples)
 
 
-                # we define the buffer for transfer and start the DMA transfer
-                print("Starting the DMA transfer and waiting until data is in board memory\n")
-                spcm_dwDefTransfer_i64 (hCard, SPCM_BUF_DATA, SPCM_DIR_PCTOCARD, lNotifySize_bytes, pvBuffer, uint64 (0), qwBufferSize)
-                spcm_dwSetParam_i32 (hCard, SPC_DATA_AVAIL_CARD_LEN, qwBufferSize)
-                spcm_dwSetParam_i32 (hCard, SPC_M2CMD, M2CMD_DATA_STARTDMA | M2CMD_DATA_WAITDMA)
-                print("... data has been transferred to board memory\n")
+            # we define the buffer for transfer and start the DMA transfer
+            print("Starting the DMA transfer and waiting until data is in board memory\n")
+            spcm_dwDefTransfer_i64 (self.hCard, SPCM_BUF_DATA, SPCM_DIR_PCTOCARD, self.lNotifySize_bytes, self.pvBuffer, uint64 (0), self.qwBufferSize)
+            spcm_dwSetParam_i32 (self.hCard, SPC_DATA_AVAIL_CARD_LEN, self.qwBufferSize)
+            spcm_dwSetParam_i32 (self.hCard, SPC_M2CMD, M2CMD_DATA_STARTDMA | M2CMD_DATA_WAITDMA)
+            print("... data has been transferred to board memory\n")
 
 
-                # now we start the generation and wait for the interrupt that signalizes the end
-                spcm_dwSetParam_i32 (hCard, SPC_M2CMD, M2CMD_CARD_START | M2CMD_CARD_ENABLETRIGGER | M2CMD_CARD_WAITREADY)
-
-            except KeyboardInterrupt:
-                return -1
+            # now we start the generation and wait for the interrupt that signalizes the end
+            spcm_dwSetParam_i32 (self.hCard, SPC_M2CMD, M2CMD_CARD_START | M2CMD_CARD_ENABLETRIGGER | M2CMD_CARD_WAITREADY)
+            return 0
+        except KeyboardInterrupt:
+            return -1
 
     def stop(self):
         # send the stop command
         try:
-            dwError = spcm_dwSetParam_i32 (hCard, SPC_M2CMD, M2CMD_CARD_STOP | M2CMD_DATA_STOPDMA)
+            dwError = spcm_dwSetParam_i32 (self.hCard, SPC_M2CMD, M2CMD_CARD_STOP | M2CMD_DATA_STOPDMA)
             print(dwError)
-            spcm_vClose (hCard)
+            print("Card has been Stopped")
+            spcm_vClose (self.hCard)
             return 0
         except Exception as e:
             print("Exception",str(e), " has occured")
@@ -139,12 +139,14 @@ class M4i6622:
 
 
 
-M4i = M4i6622()
+M4i = M4i6622(Loops=10)
 
 r = M4i.setSoftwareBuffer()
 
-if r == 0:
-    r = M4i.calculate()
+
+r = M4i.calculate(channel = 0)
+
+time.sleep(10)
 
 if r == 0:
     r = M4i.stop()
